264cac9fb8139bd0b8d43993a097fea43ca45fca
Daniele Pestilli <byakugan.87@gmail.com>
2012-04-15 18:56:54 +0200

    Added stats functionality and fixed prompt

--- JobTwit.py
+++ JobTwit.py
@@ -12,22 +12,64 @@ Date: mid-April 2012
 
 q = ['PHP', 'Python', 'Java',
 	'Objective C', 'C', 'C++', 'C#',
-	'SQL', 'MySQL', 'Android', 'Ruby',
+	'SQL', 'MySQL', 'Ruby',
 	'JavaScript', 'Action Script', 'ASP']
+
 t = {}			# Dictionary of Tweets
 links = []		# The links within the tweets
 
-def parseTweets(query):
+def getTweets(q):
 	seed = "http://search.twitter.com/search.json?q=Job%20"
+	rtype = '&result_type=mixed'
 	pages = npages()	# The number of pages we inspect (max 15)
 	rpp = 100			# The number of results per page we inspect (max 100)
-	rtype = '&result_type=mixed'
 	while pages >= 1:
-		search = urllib.urlopen(seed+query+'&rpp='+str(rpp)+'&page='+str(pages)+rtype)
+		search = urllib.urlopen(seed+q+'&rpp='+str(rpp)+'&page='+str(pages)+rtype)
 		dict = simplejson.loads(search.read())
 		for result in dict["results"]: # Result is a list of dictionaries
 			groupTweets(result['text'], result['from_user'])
 		pages -= 1
+
+# This function groups identical Tweets to avoid having them being 
+# reprinted on the screen.
+def groupTweets(tweet, user):
+	if tweet in t:
+		t[tweet][0] += 1
+		t[tweet][1].append(user)
+	else:
+		t[tweet] = [1, [user]]
+	return t
+
+# Gives statistics of the various programming languages.
+# Number of jobs per language and estimated language popularity.
+def lang_stats():
+	print 'Gathering statistics... this may take a few minutes'
+	stats = {}
+	for lang in q:
+		global t
+		t = {}
+		c = 0
+		getTweets(lang)
+		for k in t:
+			if lang.lower() in k.lower():
+				c += 1
+		stats[lang] = c
+	totaljobs = 0
+	for key in stats:
+		totaljobs += stats[key]
+	print '\nEstimated Popularity:'
+	for key, val in sorted(stats.iteritems(), key=lambda (k,v): (v,k)):
+		popularity = round(val / float(totaljobs) * 100, 2)
+		if popularity < 10:
+			if len(str(popularity)) >= 4:
+				tabs = '\t\t'
+		else:
+			tabs = '\t'
+		print '(' + red(str(popularity)) + '%)' + tabs + yellow(key) + \
+		' = ' + green(str(val)) + ' jobs'
+
+def parseTweets(query):
+	getTweets(query)
 	c = 0
 	for k in t:
 		if 'http' in k:
@@ -60,17 +102,6 @@ def red(s):
 def clear():
 	print('\x1B[2J')
 
-
-# This function groups identical Tweets to avoid having them being 
-# reprinted on the screen.
-def groupTweets(tweet, user):
-	if tweet in t:
-		t[tweet][0] += 1
-		t[tweet][1].append(user)
-	else:
-		t[tweet] = [1, [user]]
-	return t
-
 # Validate user input.
 def check_lang(n):
 	n -= 1
@@ -96,6 +127,7 @@ def npages():
 				return i
 		except ValueError:
 			print 'Only a value from 1 to 15 is accepted here.'
+		
 
 def check_location():
 	print '\nWould you like to specify a location? (ex. NYC, London)'
@@ -118,7 +150,8 @@ def check_location():
 def crawl_urls(l):
 	list_length = len(l)
 	if list_length > 0:
-		print '\n\nI\'ve collected a \033[1;32mlist of ' + str(list_length) + ' links\033[1;m from the above Tweets'
+		print '\n\nI\'ve collected a ' + \
+		green('list of ' + str(list_length) + ' links ') + 'from the above Tweets'
 		print '\nI can tell you more about them.'
 		print 'Would you like me to look through the collected URLs?'
 		print '1) No\n2) Yes'
@@ -159,20 +192,40 @@ def get_urls(s):
 		if 'http' in e:
 			links.append(e)
 
+
+
 # Prompt the user for the Tweets to search for.
 def prompt():
-	print 'What language would you like to search for?'
-	i = 1
-	for entry in q:
-		print i,')',entry
-		i += 1
-	num = check_lang(i) - 1
-	loc = check_location()
-	if loc == '':
-		query = q[num]
-	else:
-		query = q[num] + ' ' + loc
-	parseTweets(query)
-	crawl_urls(links)
+	print green('\t\t\tWelcome to JobTwit! :D')
+	print '\nWould you like to:'
+	print '1) Query Twitter for a particlar programming language\n' + \
+	'2) View general statistics'
+	prompt = '---> '
+	while True:
+		try:
+			i = int(raw_input(prompt))
+			if i == 1:
+					print 'What language would you like to search for?'
+					i = 1
+					for entry in q:
+						print i,')',entry
+						i += 1
+					num = check_lang(i) - 1
+					loc = check_location()
+					if loc == '':
+						query = q[num]
+					else:
+						query = q[num] + ' ' + loc
+					parseTweets(query)
+					crawl_urls(links)
+					break
+			elif i == 2:
+				lang_stats()
+				break
+			else:
+				print 'Only a value of 1 oe 2 is accepted here.'
+		except ValueError:
+			print 'Only a value of 1 or 2 is accepted here.'
+
 clear()
 prompt()
